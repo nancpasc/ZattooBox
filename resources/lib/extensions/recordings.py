338a6d39ec4c232044ee549be62e1c0a1c6722f1
Adam Price <github@adam.pappnase.co.uk>
2021-05-18 16:42:21 +0200

    Replace hls5 with hls7

--- resources/lib/extensions/recordings.py
+++ resources/lib/extensions/recordings.py
@@ -13,49 +13,48 @@ import os
 
 class Recordings(ZBExtension):
 
-	def init(self):
-		return
+        def init(self):
+                return
 
-	def get_items(self):
-		content = [
-			ZBFolderItem(
-				host=self,
-				args={'mode': 'root'},
-				title=self.ZBProxy.get_string(30102),
-				image=os.path.join(self.ExtensionsPath, 'recordings/video.png')
-			)
-		]
-		return content
+        def get_items(self):
+                content = [
+                        ZBFolderItem(
+                                host=self,
+                                args={'mode': 'root'},
+                                title=self.ZBProxy.get_string(30102),
+                                image=os.path.join(self.ExtensionsPath, 'recordings/video.png')
+                        )
+                ]
+                return content
 
-	def activate_item(self, args):
-		if args['mode'] == 'root':
-			self.build_recordingsList()
-		elif args['mode'] == 'watch':
-			self.watch(args)
+        def activate_item(self, args):
+                if args['mode'] == 'root':
+                        self.build_recordingsList()
+                elif args['mode'] == 'watch':
+                        self.watch(args)
 
-	#---
+        #---
 
-	def build_recordingsList(self):
-		resultData = self.ZapiSession.exec_zapiCall('/zapi/playlist', None)
-		if resultData is None:
-			return
+        def build_recordingsList(self):
+                resultData = self.ZapiSession.exec_zapiCall('/zapi/playlist', None)
+                if resultData is None:
+                        return
 
-		recordings = []
-		for record in resultData['recordings']:
-			recordings.append(ZBPlayableItem(
-				host=self,
-				args={'mode': 'watch', 'id': record['id']},
-				title=record['title'],
-				image=record['image_url'],
-				title2=record['episode_title']
-				)
-			)
-		self.ZBProxy.add_directoryItems(recordings)
+                recordings = []
+                for record in resultData['recordings']:
+                        recordings.append(ZBPlayableItem(
+                                host=self,
+                                args={'mode': 'watch', 'id': record['id']},
+                                title=record['title'],
+                                image=record['image_url'],
+                                title2=record['episode_title']
+                                )
+                        )
+                self.ZBProxy.add_directoryItems(recordings)
 
-	def watch(self, args):
-		params = {'recording_id': args['id'], 'stream_type': 'hls5'}
-		resultData = self.ZapiSession.exec_zapiCall('/zapi/watch', params)
-		if resultData is not None:
-			url = resultData['stream']['url']
-			self.ZBProxy.play_stream(url)
-
+        def watch(self, args):
+                params = {'recording_id': args['id'], 'stream_type': 'hls7'}
+                resultData = self.ZapiSession.exec_zapiCall('/zapi/watch', params)
+                if resultData is not None:
+                        url = resultData['stream']['url']
+                        self.ZBProxy.play_stream(url)
